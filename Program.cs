444e3dbf9455bb775509b3076221fe18e8e3b1d3
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 08:59:08 +0100

    Opgave 7
    Opgave fra opgave sæt

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Opgave_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+        }
+
+        static void Welcome()
+        {
+            Console.WriteLine("Enter your name!");
+            string? name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hello Welcome to this opgave {0}!", name);
+        }
+
+    }
+}
