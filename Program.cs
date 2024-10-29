95759762d637e740bdb56bf81218f28a8c43765f
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 09:03:14 +0100

    Opgave 12
    Opgave fra opgave sæt

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Opgave_12
+{
+    internal class Program
+    {
+        static string email;
+        static bool valid = false;
+        static void Main(string[] args)
+        {
+            emailVerify();
+        }
+
+        static void emailVerify()
+        {
+            while (!valid)
+            {
+                Console.WriteLine("\nYou need to enter @gmail.com");
+                Console.Write("Enter your email : ");
+                email = Console.ReadLine();
+
+                if (email.Contains("@gmail.com"))
+                {
+                    valid = true;
+                    Console.Clear();
+                    Console.WriteLine("Thank you for the Gmail! My good sir");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Fejl: Beskeden skal indholde @gmail.com");
+                    return;
+                }
+
+            }
+
+        }
+    }
+}
