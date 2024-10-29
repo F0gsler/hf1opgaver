f61c6b1760f1f34ef17cdc8ac15b47fd2f4d7c5a
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 09:03:47 +0100

    Opgave 15
    Opgave fra opgave sæt

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Opgave_15
+{
+    internal class Program
+    {
+
+        static double tal1 = Convert.ToDouble(Console.ReadLine());
+        static double tal2 = Convert.ToDouble(Console.ReadLine());
+        static double tal3 = Convert.ToDouble(Console.ReadLine());
+
+        static double[] numbers = { tal1, tal2, tal3 };
+
+        static void Main(string[] args)
+        {
+            calc();
+        }
+
+        static void calc()
+        {
+            Console.WriteLine("Enter your numbers");
+            Array.Sort(numbers);
+            Console.WriteLine("Numbers in order: {0} {1} {2} ", numbers[0], numbers[1], numbers[2]);
+        }
+
+    }
+}
