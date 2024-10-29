7f3c720d70b3c7c072e1163152cb75dcda355ba1
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 09:01:50 +0100

    Opgave 10
    Denne opgave skippet jeg

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Opgave10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vi skipper denne");
+            Console.WriteLine("Hilsen Marius Ulslev Fogelgren");
+        }   
+    }
+}
