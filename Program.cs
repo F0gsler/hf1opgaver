691ca880e85d83151003050ec1aed7141b27f6d1
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 09:55:25 +0100

    Opgave 16

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Opgave_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bmicalc();
+        }
+
+        static void bmicalc()
+        {
+            Console.WriteLine("Enter your weigth in KG: ");
+            double weigth = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter your height in Meters: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your BMI is : {0}", weigth / (height * height));
+
+        }
+    }
+}
