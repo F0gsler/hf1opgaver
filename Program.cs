fc02cbb279b74ef09f206df4ebdecf1481a39c63
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 08:55:30 +0100

    Opgave 4
    Opgave fra opgave sæt

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace opgave_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alder = Convert.ToInt32(Console.ReadLine());
+
+            if (alder >= 18 )
+            {
+                Console.WriteLine("Du er møndig!");
+            }
+
+            else
+            {
+                Console.WriteLine("Du er ikke Møndig!");
+            }
+        }
+    }
+}
