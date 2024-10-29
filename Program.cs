5470f9632de864d122d3f43846a61d691b5b94ac
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 08:51:56 +0100

    Opgave 2
    Opgave 2 fra opgave sæt

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿    namespace Opgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? navn = Console.ReadLine();
+            Console.WriteLine(navn);
+        }
+    }
+}
