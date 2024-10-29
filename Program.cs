2e23be87529cb8f612480e917e3838c9f661e097
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-10-29 08:54:29 +0100

    Opgave 3
    Opgave 3 fra opgave sæt

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Opgave_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            tal();
+        }
+
+        public static void tal()
+        {
+        int tal1 = Convert.ToInt32(Console.ReadLine());
+        int tal2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Added: {0}",tal1 + tal2);
+            Console.WriteLine("Subtractet: {0}", tal1 - tal2);
+            Console.WriteLine("Multiplyed: {0}", tal1 * tal2);
+            Console.WriteLine("Divived: {0}", tal1 / tal2);
+
+        }
+    }
+}
